a81ffd34828215408797bf5e607d39dd19206b74
nico-php-coder <69898039+nico-php-coder@users.noreply.github.com>
2021-07-06 11:30:04 +0200

    add case model

---
+++ public/js/case.js
@@ -0,0 +1,20 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Case = void 0;
+class Case {
+    constructor(abscisse, ordonnee, isPassable, imgName) {
+        this.abscisse = abscisse;
+        this.ordonnee = ordonnee;
+        this.isPassable = isPassable;
+        this.imgName = imgName;
+    }
+    getAbscisse() { return this.abscisse; }
+    getOrdonnee() { return this.ordonnee; }
+    getIsPassable() { return this.isPassable; }
+    getImgName() { return this.imgName; }
+    setAbscisse(value) { this.abscisse = value; }
+    setOrdonnee(value) { this.ordonnee = value; }
+    setIsPassable(value) { this.isPassable = value; }
+    setImgName(value) { this.imgName = value; }
+}
+exports.Case = Case;
